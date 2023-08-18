49b5d5bc0695f4fca8716f9d353965c9400de8d4
Daria Juniper <daria@deadletteroffice.ca>
2023-08-17 23:46:55 -0400

    * readability improvements * beta has been improved and cleaned up, config file added for usability * datetime sorting bug with sqlite fix

--- README.md
+++ README.md
@@ -1,16 +1,72 @@
 # NodeWall
-BBS Style Graffiti Wall made for BPQ Application
+### Graffiti Wall for BPQ
+---
+[MIT License](https://opensource.org/license/mit/)
 
-I put this together for my BPQ Node.  It runs from xinet.d, I've included the xinet.d file....and yeah, here is how you should configure it in bpq32.conf.
+NodeWall is a Graffiti Wall for Packet Radio.  It's made to work with the [BPQ32 Packet Switch from G8BPQ](https://www.cantab.net/users/john.wiseman/Documents/BPQ32.html).  It requires that you have a basic Python3 install which most everyone should who runs modern Linux.
 
-bpq32.conf appl line:
-APPLICATION 10,WALL,C 15 HOST 1 S
+NodeWall when running on your packet system will look like this:
+```
+*** Connected to WALL        
+-=- Node Wall -=-
 
-Where 15 is your Telnet port and 1 is the port number (zero indexed) from the following list:
-CMDPORT=6000 6001 6002 6003 6004
+Showing entries 1-10 of 32:
+(16-Aug 00:10) < PD1NL > I'm using Windows so i can see through walls
+(15-Aug 09:32) < WW6Q > Just making my wall rounds.... carry on
+(14-Aug 03:27) < AG7BI > This is cool! Have to add it to my node
+(13-Aug 07:38) < PE1RRR > @BCX <3 thanks for the msg do you have a twitch channel?
+(13-Aug 04:50) < YD0BCX-15 > Just passing by while streaming to twicth xixi
+(12-Aug 16:15) < PE1RRR > @M0JQQ Thx for visiting, Robin :D Condx on 40 lately have been transient & turbulent!
+(12-Aug 09:15) < M0JQQ > Morning Red. Yr mail received. This via MM3NDH. No connect via GB7IOW this morning. Will try later 73
+(11-Aug 08:15) < WW6Q > Yes, this is where you share your kitty wittys.
+(11-Aug 06:21) < EI2GYB > is this where i write somthing witty?
+(06-Aug 22:04) < WW6Q > i tot i taw a puddy tat.
 
-which would be port 6001 and you can define this in the CONFIG pragma of the Telnet PORT definition.
+[P]ost a message [B]ack [F]orward [D]elete E[x]it
+```
 
+## Installation
+Download and install NodeWall to your system:
+```
+git clone https://github.com/juniberry/NodeWall.git
+```
 
-Enjoy!
+Configure BPQ to connect to external application ports[^1]:
+```
+; Telnet Port
+PORT
+PORTNUM=15
+ ID=Telnet
+ DRIVER=TELNET
+ ; ... etc ...
+ CONFIG
+  ; external application ports, zero indexed!
+  CMDPORT=6000 6001 6002 6003 6004 6005 6006 6007 6008 6009 6010
+ENDPORT
 
+; Application Lines
+APPLICATION 1,BBS,,N0CALL-1,CALBBS,255
+APPLICATION 3,CHAT,,N0CALL-11,CALCHT,255
+
+; External Applications
+APPLICATION 10,WALL,C 15 HOST 1 S,N0CALL-14,CALWAL,255
+```
+Note: CMDPORT= ports are zero indexed, such that "C 15 HOST 1 S" will connect to port 15 (Telnet) in the example config, and then connect to local host on command port 1 which is the second port 6001 in the CMDPORT= list. **The "S" in the connect string is needed so that BPQ passes the users Callsign to NodeWall.**
+
+You can run NodeWall from inet.d or xinet.d as a TCP service[^2]:
+```
+service wall
+{
+	disable		= no
+	protocol	= tcp
+	port		= 6001
+	server		= /home/bpquser/wall.py
+	user		= bpquser
+	socket_type	= stream
+	wait		= no
+}
+```
+
+---
+[^1]: [LinBPQ Applications Interface](https://www.cantab.net/users/john.wiseman/Documents/LinBPQ%20Applications%20Interface.html)
+[^2]: [xinet.d](https://en.wikipedia.org/wiki/Xinetd)
--- wall.py
+++ wall.py
@@ -1,16 +1,41 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
-# wall.py
-# Packet Node Graffiti Wall
-# digitaljackalope@github Mar-2023
-# ...nothing fancy...
+"""
+wall.py
+
+Title: NodeWall
+Description: 
+Author: Daria Juniper -- juniberry@github
+Created: March 2023
+Version: 1.0
+
+
+Changelog:
+* readability improvements
+* beta has been improved and cleaned up, config file added for usability
+* datetime sorting bug with sqlite fix
+"""
 
 import sys
+import os
 import sqlite3
 import datetime
+import configparser
+
+# get the directory path of the running script
+script_path = os.path.dirname(os.path.abspath(__file__))
+
+# build paths to required files
+config_file_path = os.path.join(script_path, 'wall.ini')
+db_file_path = os.path.join(script_path, 'wall.db')
+
+# load config file
+config = configparser.ConfigParser()
+config.read(config_file_path)
 
 # connect to the database
-conn = sqlite3.connect('/home/bpquser/wall.db')
+conn = sqlite3.connect(db_file_path)
 c = conn.cursor()
 
 # create a table for the wall messages
@@ -22,11 +47,11 @@ c.execute('''CREATE TABLE IF NOT EXISTS messages
 
 # get the user's callsign
 callsign = input().strip()
-print("-=- Node Wall -=-")
+print(config['wall']['banner'])
 
 # show the latest entries on the wall
-num_entries = 10  # change this to control the number of entries shown
-max_message_length = 140  #change this to the maxium wall entry size you allow
+num_entries = config['posts'].getint('perpage')
+max_message_length = config['posts'].getint('maxlen')
 
 start_index = 0
 
@@ -34,15 +59,18 @@ while True:
     c.execute("SELECT COUNT(*) FROM messages")
     num_messages = c.fetchone()[0]
 
-    c.execute("SELECT * FROM messages ORDER BY timestamp ASC LIMIT ? OFFSET ?", (num_entries, start_index))
+    if start_index <= 0:
+        start_index = 0
+
+    c.execute("SELECT * FROM messages ORDER BY timestamp DESC LIMIT ? OFFSET ?", (num_entries, start_index))
     rows = c.fetchall()
     if rows:
-        print("\nShowing entries {0}-{1} of {2}:".format(start_index+1, start_index+len(rows), num_messages))
+        print("\nPosts {0}-{1} of {2}:".format(start_index+1, start_index+len(rows), num_messages))
         for row in rows:
             date_string = datetime.datetime.strptime(row[3], '%Y-%m-%d %H:%M:%S').strftime('(%d-%b %H:%M)')
-            print(date_string,"<",row[1], ">", row[2])
+            print(date_string, "<", row[1], ">", row[2])
     else:
-        print("\nNo more entries to show.")
+        print("\nNo more posts to show.")
 
     # present options to the user
     print("\n[P]ost a message [B]ack [F]orward [D]elete E[x]it")
@@ -50,20 +78,19 @@ while True:
 
     if choice == "p":
         # ask the user for a message
-        message = input("Enter your message (limit {0} characters): ".format(max_message_length)).strip()[:max_message_length]
+        message = input("Enter your post (limit {0} characters): ".format(max_message_length)).strip()[:max_message_length]
 
         if message:
             # Check if the message starts with "*** Disconnected from"
             if message.startswith("*** Disconnected from"):
-                print("Disconnect detected, ignoring post.")
                 conn.close()
                 sys.exit()
             # insert the message into the database
             c.execute("INSERT INTO messages (callsign, message) VALUES (?, ?)", (callsign, message))
             conn.commit()
-            print("Message posted to the wall.")
+            print("Posted to the wall.")
         else:
-            print("Message was not posted as it was empty or null.")
+            print("Not posted as it was empty.")
     elif choice == "b":
         # move the start index back by num_entries
         start_index = max(0, start_index - num_entries)
@@ -75,17 +102,17 @@ while True:
         c.execute("SELECT * FROM messages WHERE callsign=? ORDER BY timestamp DESC LIMIT 1", (callsign,))
         row = c.fetchone()
         if row:
-            confirm = input("Are you sure you want to delete your latest message? (y/n): ").strip().lower()
+            confirm = input("Delete your latest post? (y/n): ").strip().lower()
             if confirm == "y":
                 c.execute("DELETE FROM messages WHERE id=?", (row[0],))
                 conn.commit()
-                print("Message deleted.")
+                print("Post deleted.")
         else:
-            print("You have not posted any messages.")
+            print("You have not posted.")
     elif choice == "x":
-        # exit the program
+        # cleanup and exit
         conn.close()
-        print("Returning to Node.")
+        print(config['wall']['exitmsg'])
         break
     else:
-        print("Invalid choice. Please try again.")
+        print("Invalid choice.")
